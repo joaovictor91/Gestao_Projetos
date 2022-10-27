94d973a2213ded73c71af5dd82c714a505dd6621
joaovictor91 <j-victor-franco@live.com>
2022-10-27 01:50:08 -0200

    Atividade Finalizada corretamente & Criando a Tabuada

--- relogio/src/Horas.js
+++ relogio/src/Horas.js
@@ -1,8 +1,41 @@
+import Typewriter from 'typewriter-effect';
+
 function Horas(){
+    let dianome = new Array ("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
+    let mes = (new Date().getMonth() + 1);
+    let nomemes;
+    
+    if(mes===1){nomemes = "Janeiro";}
+        if(mes===2){nomemes = "Fevereiro";}
+        if(mes===3){nomemes = "Março";}
+        if(mes===4){nomemes = "Abril";}
+        if(mes===5){nomemes = "Maio";}
+        if(mes===6){nomemes = "Junho";}
+        if(mes===7){nomemes = "Julho";}
+        if(mes===8){nomemes = "Agosto";}
+        if(mes===9){nomemes = "Setembro";}
+        if(mes===10){nomemes = "Outubro";}
+        if(mes===11){nomemes = "Novembro";}
+        if(mes===12){nomemes = "Dezembro";}
+    
     return (
+        
+        
+        
         <div>
-            
+            <h1>Hoje é {dianome[new Date().getDay()] + ' Dia ' + new Date().getDate()+ ' do mês ' 
+            + nomemes + ' do ano de ' + new Date().getFullYear()}.</h1>
             <h2>Agora é {new Date().toLocaleTimeString()}.</h2>
+            <h1>
+          <Typewriter 
+            onInit={(typewriter) =>{
+                    typewriter.typeString('Venha para FATEC')
+                    
+                    .deleteAll()
+                    .start();
+            }}
+          />
+          </h1>
         </div>
     );
     
--- tabuada/src/index.js
+++ tabuada/src/index.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Horas from './Horas';
-// Não precisei utilizar
-//import Data from './Data';
-//import Baner from './Baner';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function Hours(){
-  root.render(
-    <Horas/>
-  );
-}
-setInterval(Hours,1000);
-//Por hora não está funcionando
-/*root.render(
-  <div>
-    <Data></Data>
-    <p></p>
-    <Horas></Horas>
-    <p></p>
-    <Baner></Baner>
-  </div>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-*/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
